56f7db1d6a3ec47e53802eaf8a169164b7cf45b5
eric <ericarterbridge@gmail.com>
2022-08-03 15:57:38 -0400

    finished lab

--- src/main/java/zipcode/Hamming.java
+++ src/main/java/zipcode/Hamming.java
@@ -7,18 +7,21 @@ public class Hamming {
     public Hamming(String s, String s1) {
         this.s = s;
         this.s1 = s1;
+
+        if (s.length() != s1.length()) {
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        }
     }
 
     public int getHammingDistance() {
+
+
         int count = 0;
-        if (s.length() == s1.length()){
-            for (int i = 0; i < s.length(); i++){
-                if (s.charAt(i) != s1.charAt(i)){
-                    count++;
-                }
+        for (int i = 0; i < s.length(); i++){
+            if (s.charAt(i) != s1.charAt(i)){
+                count++;
             }
-            return count;
         }
-        return -1;
+        return count;
     }
 }
